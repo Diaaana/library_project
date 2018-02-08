9e4578edb1b587b1b4e48c03b7c93fd8966c6732
Diana <diana190897_7@mail.ru>
2018-02-08 22:19:37 +0300

    fixed change locale

--- src/main/java/by/radomskaya/project/command/common/ChangeLocaleCommand.java
+++ src/main/java/by/radomskaya/project/command/common/ChangeLocaleCommand.java
@@ -1,6 +1,7 @@
 package by.radomskaya.project.command.common;
 
 import by.radomskaya.project.command.Command;
+import by.radomskaya.project.constant.JspPageConstants;
 import by.radomskaya.project.constant.ParameterConstants;
 import by.radomskaya.project.controller.Router;
 import by.radomskaya.project.exception.CommandException;
@@ -13,10 +14,13 @@ public class ChangeLocaleCommand implements Command {
     @Override
     public Router execute(HttpServletRequest request) throws CommandException {
         Router router = new Router();
-        String page = request.getParameter(ParameterConstants.PARAM_URL);
         HttpSession session = request.getSession();
+        String page;
+
         String locale = request.getParameter(ParameterConstants.PARAM_LOCALE);
         session.setAttribute(ParameterConstants.PARAM_LOCALE, locale);
+        page = JspPageConstants.START_PAGE;
+
         router.setPagePath(page);
         router.setRoute(Router.RouteType.FORWARD);
         return router;
